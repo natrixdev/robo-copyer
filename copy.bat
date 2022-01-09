8d236a630e68d222982a9977baef1848a449723e
natrix_dev <discordlenko@gmail.com>
2022-01-09 11:07:34 +0100

    Create copy.bat

---
+++ copy.bat
@@ -0,0 +1,13 @@
+title Here we are !
+@echo off input Pc name ?
+@echo off input Copy % ? (100%, 50%, 25%)
+
+cls 
+color a 
+
+%Rocopy --Copy%
+
+cls 
+tilte Done !
+color c 
+@echo off Done ! with success
